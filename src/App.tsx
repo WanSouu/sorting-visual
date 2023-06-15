5210a236a4e79f273eac7ad4823af1d52c2bbe05
WanSouu <wansolek@gmail.com>
2023-06-15 15:25:28 +0100

    Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { ThemeProvider } from './ThemeProvider';
-import { Welcome } from './Welcome/Welcome';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <Welcome />
+      
     </ThemeProvider>
   );
 }
